fb485a2b605afb2f0e3c4d1302fad841ab31edf3
Doug Anson <doug.anson@arm.com>
2021-05-11 18:18:19 -0500

    updated rev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -26,14 +26,9 @@ setup(
     author_email="Doug.Anson@pelion.com",
     license='Apache 2.0',
     classifiers=(
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python',
