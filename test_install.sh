c8cd2c9d67a18d8c483a92ff57bdfd3acfbf068c
Doug Anson <doug.anson@arm.com>
2021-05-12 16:10:20 -0500

    updated test script

--- test_install.sh
+++ test_install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -x
-
 python3 -m pip uninstall -y pelion_sagemaker_controller
 echo "python3 -m pip install pelion_sagemaker_controller"
 echo "python3 -m pip install --index-url https://test.pypi.org/simple/ pelion_sagemaker_controller"
