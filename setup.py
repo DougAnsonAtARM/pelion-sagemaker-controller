4257942ef5d0ff43879323b7c55dee61e8051418
Doug Anson <doug.anson@arm.com>
2021-07-06 11:21:58 -0500

    cleanups

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 import os
 from setuptools import setup
 
-__version__ = '0.0.12'
+__version__ = '0.0.13'
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
